a10aff03af86567c0600229723470523d7aed4eb
Ry-an7 <117701680+Ry-an7@users.noreply.github.com>
2024-10-02 20:24:35 -0700

    added my own vis to test

--- vis.js
+++ vis.js
@@ -1,20 +1,26 @@
 // Create and render the bar chart
 // async function to load data from datasets/videogames_long.csv using d3.csv and then make visualizations
-async function render() {
+async function renderBar() {
   // load data
-  const data = await d3.csv("./dataset/videogames_wide.csv");
+  const data = await d3.csv("./dataset/videogames_long.csv");
 
   // create a bar chart
-  const vlSpec = vl
-    .markBar()
-    .data(data)
-    .encode(
-      vl.y().fieldN("Platform").sort("-x"),
-      vl.x().fieldQ("Global_Sales").aggregate("sum")
-    )
-    .width("container")
-    .height(400)
-    .toSpec();
+const vlSpec = vl
+  .markBar()
+  .data(data)
+  .encode(
+  vl.x().fieldN("platform")
+    .title("Platform"),
+  vl.y().fieldQ("global_sales").aggregate("sum")
+    .title("Global Sales"),
+  vl.color().fieldN("genre")
+    .title("Genre")
+    .scale({scheme : "category20"}) 
+  )
+
+  .width("container")
+  .height(400)
+  .toSpec();
 
   vegaEmbed("#view", vlSpec).then((result) => {
     const view = result.view;
@@ -22,4 +28,29 @@ async function render() {
   });
 }
 
-render();
+async function renderLine() {
+  // load data
+  const data = await d3.csv("./dataset/videogames_long.csv");
+
+  // create a bar chart
+const vlSpec = vl
+.markLine()
+.data(data)
+.encode(
+  vl.x().fieldN("year"),
+  vl.y().fieldQ("global_sales").aggregate("sum"),
+  vl.color().fieldN("genre")
+)
+
+  .width("container")
+  .height(400)
+  .toSpec();
+
+  vegaEmbed("#views", vlSpec).then((result) => {
+    const view = result.view;
+    view.run();
+  });
+}
+
+renderBar();
+renderLine();
